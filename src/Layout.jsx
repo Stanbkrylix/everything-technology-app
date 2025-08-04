8040b23febcf71fff2b28e169282615743fc5bab
Stanley Delva <juniordelva1@gmail.com>
2025-08-04 05:56:59 -0400

    setting up main pages and component

---
+++ src/Layout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+function Layout({ Navbar }) {
+    return (
+        <div className="main-container">
+            {/* Navbar */}
+            <div className="content-container"></div>
+        </div>
+    );
+}
+
+export default Layout;
---
+++ src/Pages/NewPost.jsx
@@ -0,0 +1,9 @@
+function NewPost() {
+    return (
+        <div className="new-post-container">
+            <h1>Create New Post</h1>
+        </div>
+    );
+}
+
+export default NewPost;
